78d4db95a60d7c56af56e33719f03d7f899f08fd
Pierre Convert <pierreconvert@192.168.1.14>
2025-01-26 17:14:44 +0100

    Updating build.js

--- build.js
+++ build.js
@@ -3,17 +3,26 @@ const ROOT = build({
 	dependencies: [
 		"S8-api",
 		"S8-core-io-xml",
-		"S8-core-io-joos",
+		"S8-core-io-JSON",
 		"S8-core-io-bytes",
-		"S8-core-arch-silicon",
-		"S8-core-arch-magnesium",
-		"S8-core-web-helium",
-		"S8-core-web-carbon",
+		
 		"S8-core-bohr-atom",
 		"S8-core-bohr-beryllium",
 		"S8-core-bohr-lithium",
 		"S8-core-bohr-neodymium",
-		"S8-core-bohr-neon"
+		"S8-core-bohr-neon",
+		
+		"S8-core-arch-silicon",
+		"S8-core-arch-titanium",
+		
+		"S8-core-db-tellurium",
+		"S8-core-db-cobalt",
+		"S8-core-db-copper",
+		
+		"S8-core-web-helium",
+		"S8-core-web-carbon",
+		"S8-core-web-manganese",
+		
 	],
-	target: "S8-core-web-xenon",
+	target: "S8-core-web-xenon"
 });
